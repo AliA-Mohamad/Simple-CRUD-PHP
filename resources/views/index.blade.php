6ac92a1834f1296972bb533a124926b88fdcd063
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2024-05-15 16:39:54 -0300

    Adiciona bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,27 @@
-@foreach ($tasks as $task)
-    <div>
-        <p> {{ $task->id }}  |  {{ $task->name }}  |  {{ $task->description}} </p>
-    </div>
-@endforeach
+@extends('master')
 
-<a href="{{ route('create') }}"> add a task </a>
+@section('content')
+    <div class="container d-flex justify-content-center">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col"> ID </th>
+                    <th scope="col"> Name </th>
+                    <th scope="col"> Desc </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <th scope="row"> {{ $task->id }} </th>
+                        <td> {{ $task->name }} </td>
+                        <td> {{ $task->description }} </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <a href="{{ route('create') }}"> add a task </a>
+
+@endsection
