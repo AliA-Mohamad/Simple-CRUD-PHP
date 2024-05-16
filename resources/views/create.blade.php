aba9586fddb1c6b3406769350dbc8886e16f1157
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2024-05-16 14:00:16 -0300

    estilização do create com bootstrap

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,10 +1,33 @@
-<form action="{{ route('store') }}" method="post">
-    @csrf
-    <label for="name">type the task:</label>
-    <input type="text" id="name" name="name">
-    <br> 
-    <label for="description">type what you need to do</label>
-    <input type="text" id="description" name="description">
-    <br>
-    <button type="submit">save</button>
-</form>
+@extends('master')
+
+@section('content')
+
+<style>
+    .full-height {
+        height: 100vh;
+    }
+</style>
+
+<div class="d-flex container justify-content-center align-items-center full-height">
+    <div class="row border rounded-4 p-5">
+
+        <span class="mb-5 w-100 text-center fs-4">Make your task</span>
+
+        <form action="{{ route('store') }}" method="post">
+            @csrf
+            <div class="input-group input-group-sm mb-5 mt-1">
+                <span class="input-group-text"> Task </span>
+                <input type="text" class="form-control" id="name" name="name" placeholder="type...">
+            </div>
+
+            <div class="input-group input-group-sm mb-5 ">
+                <span class="input-group-text"> Description </span>
+                <input type="text" class="form-control" id="description" name="description" placeholder="type...">
+            </div>
+            <div>
+                <button type="submit" class="btn btn-success w-100">save</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,29 @@
 @extends('master')
 
 @section('content')
-    <div class="container d-flex justify-content-center">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col"> ID </th>
-                    <th scope="col"> Name </th>
-                    <th scope="col"> Desc </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
+    <div class="container-sm justify-content-center p-5">
+        <div class="row row-cols-auto">
+            <table class="table table-hover">
+                <thead>
                     <tr>
-                        <th scope="row"> {{ $task->id }} </th>
-                        <td> {{ $task->name }} </td>
-                        <td> {{ $task->description }} </td>
+                        <th scope="col"> ID </th>
+                        <th scope="col"> Name </th>
+                        <th scope="col"> Desc </th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <th scope="row"> {{ $task->id }} </th>
+                            <td> {{ $task->name }} </td>
+                            <td> {{ $task->description }} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <a href="{{ route('create') }}" class="col align-self-end btn btn-primary"> add a task </a>
+        </div>
     </div>
 
-    <a href="{{ route('create') }}"> add a task </a>
 
 @endsection
