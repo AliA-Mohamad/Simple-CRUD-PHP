ad90659e2c107c235ba551cd179a88a09e720fa8
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2024-05-24 17:25:31 -0300

    botão de deletar e botão de editar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,20 @@
     <div class="container-sm justify-content-center p-5">
         <div class="row row-cols-auto">
             <table class="table table-hover">
+                <colgroup>
+                    <col style="width: 5%;">
+                    <col style="width: 15%;">
+                    <col style="width: 50%;">
+                    <col style="width: 30%;">
+                </colgroup>
                 <thead>
                     <tr>
                         <th scope="col"> ID </th>
                         <th scope="col"> Name </th>
                         <th scope="col"> Desc </th>
+                        <th scope="col" class=" text-center"> 
+                            <a href="{{ route('create') }}" class="btn btn-primary"> add a task </a>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -17,11 +26,14 @@
                             <th scope="row"> {{ $task->id }} </th>
                             <td> {{ $task->name }} </td>
                             <td> {{ $task->description }} </td>
+                            <td class="text-center"> 
+                                <a href="" class="btn btn-warning">▢</a>
+                                <a href="" class="btn btn-danger">✕</a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
-            <a href="{{ route('create') }}" class="col align-self-end btn btn-primary"> add a task </a>
         </div>
     </div>
 
